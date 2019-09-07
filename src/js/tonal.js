b95abde80123c36e7745e5c547f1642377936a41
Steven Cobb <sjcobb.dev@gmail.com>
2019-09-07 06:10:00 -0400

    WebMidi enable and log notes from MPK mini play getInputByName, pitchbend ex

---
+++ src/js/tonal.js
@@ -0,0 +1,10 @@
+// // https://github.com/tonaljs/tonal
+// import { note, interval, transpose, distance } from "@tonaljs/tonal";
+
+// note("A4").midi; // => 60
+// note("a4").freq; // => 440
+// note("c#2").accidentals; // => '#'
+// note("x").midi; // => undefined
+// interval("5P").semitones; // => 7
+// transpose("C4", "5P"); // => "G4"
+// distance("C4", "G4"); // => "5P"
